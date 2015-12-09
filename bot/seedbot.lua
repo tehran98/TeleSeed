1e6112d6a841f4ccf56c0850b4eafc514b41e5aa
tehran98 <ahmadihamed193@gmail.com>
2015-12-09 17:43:28 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -223,121 +223,114 @@ function create_config( )
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v1
+    about_text = [[GaMeOvEr v3
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
-
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@seyedan25 [Manager]
+@tehran980 [Founder]  [Developer] [Manager]
 
 Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
+iMaNdaneshi
 
 Our channels
-@teleseedch [English]
+@game_overbot [persian]
 ]],
     help_text = [[
 Commands list :
 
 !kick [username|id]
 You can also do it by reply
-
+-------------------------
 !ban [ username|id]
 You can also do it by reply
-
+-------------------------
 !unban [id]
 You can also do it by reply
-
+-------------------------
 !who
 Members list
-
+-------------------------
 !modlist
 Moderators list
-
+-------------------------
 !promote [username]
 Promote someone
-
+-------------------------
 !demote [username]
 Demote someone
-
+-------------------------
 !kickme
 Will kick user
-
+-------------------------
 !about
 Group description
-
+-------------------------
 !setphoto
 Set and locks group photo
-
+-------------------------
 !setname [name]
 Set group name
-
+-------------------------
 !rules
 Group rules
-
+-------------------------
 !id
 return group id or user id
-
+-------------------------
 !help
-
+-------------------------
 !lock [member|name]
 Locks [member|name] 
-
+-------------------------
 !unlock [member|name|photo]
 Unlocks [member|name|photo]
-
+-------------------------
 !set rules <text>
 Set <text> as rules
-
+-------------------------
 !set about <text>
 Set <text> as about
-
+-------------------------
 !settings
 Returns group settings
-
+-------------------------
 !newlink
 create/revoke your group link
-
+-------------------------
 !link
 returns group link
-
+-------------------------
 !owner
 returns group owner id
-
+-------------------------
 !setowner [id]
 Will set id as owner
-
+-------------------------
 !setflood [value]
 Set [value] as flood sensitivity
-
+-------------------------
 !stats
 Simple message statistics
-
+-------------------------
 !save [value] <text>
 Save <text> as [value]
-
+-------------------------
 !get [value]
 Returns text of [value]
-
+-------------------------
 !clean [modlist|rules|about]
 Will clear [modlist|rules|about] and set it to nil
-
+-------------------------
 !res [username]
 returns user id
 "!res @username"
-
+-------------------------
 !log
 will return group logs
-
+-------------------------
 !banlist
 will return group ban list
-
+-------------------------
 **U can use both "/" and "!" 
 
 
